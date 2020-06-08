c9146b6ed2b35551b234ccd1a369ca5c62309089
gonzalocarhuachin <lgonzalocarhuachin@gmail.com>
2020-06-07 21:12:47 -0500

    Pedidos APP

---
+++ precache-manifest.5e323c68dae5f823a9025cddd4bac20c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdf013aae058380359a0558254b5f572",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a5bcb869ed977b007d55",
+    "url": "/static/js/2.ea1a6b03.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ea1a6b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63e67187592d9c76c5bc",
+    "url": "/static/js/main.7b5637f5.chunk.js"
+  },
+  {
+    "revision": "ef03fad86a919ef26f97",
+    "url": "/static/js/runtime-main.3ada473c.js"
+  }
+]);
